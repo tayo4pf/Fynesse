ddc1c247cfdb89a74ffb9d716cb9e62d3ddfd7bf
tayo4pf <tyoluranti@gmail.com>
2023-11-27 18:23:59 +0000

    First Commit

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,4 +1,7 @@
 from .config import *
+import requests
+import pymysql
+import osmnx as ox
 
 """These are the types of import we might expect in this file
 import httplib2
@@ -11,7 +14,115 @@ import sqlite"""
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
-def data():
-    """Read the data from the web or local file, returning structured format such as a data frame"""
-    raise NotImplementedError
+def request_to_csv(folder, filename, url):
+    r = requests.get(url, allow_redirects=True)
+    open(f"{folder}/{filename}", "wb").write(r.content)
 
+def create_connection(user, password, host, database, port = 3306):
+    """ Create a database connection to the MariaDB database
+        specified by the host url and database name.
+    :param user: username
+    :param password: password
+    :param host: host url
+    :param database: database
+    :param port: port number
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = pymysql.connect(user=user,
+                               passwd=password,
+                               host=host,
+                               port=port,
+                               local_infile=1,
+                               db=database
+                               )
+    except Exception as e:
+        print(f"Error connecting to the MariaDB Server: {e}")
+    return conn
+
+def select_top(conn, table, n):
+    """
+    Query n first rows of the table
+    :param conn: the Connection object
+    :param table: The table to query
+    :param n: Number of rows to query
+    """
+    cur = conn.cursor()
+    cur.execute(f'SELECT * FROM {table} LIMIT {n}')
+
+    rows = cur.fetchall()
+    return rows
+
+def head(conn, table, n=5):
+    rows = select_top(conn, table, n)
+    for r in rows:
+        print(r)
+
+def upload_file(conn, filename, table):
+    cur = conn.cursor()
+    load_data = f"""LOAD DATA LOCAL INFILE '{filename}' INTO TABLE {table} FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES STARTING BY '' TERMINATED BY '\n';"""
+    cur.execute(load_data)
+
+def local_features(latitude, longitude, width_km, height_km):
+    box_width = width_km * (0.02/2.2)
+    box_height = height_km * (0.02/2.2)
+
+    north = latitude + (box_height/2)
+    south = latitude - (box_height/2)
+    west = longitude - (box_width/2)
+    east = longitude + (box_width/2)
+
+    tags = {
+        "amenity": True,
+        "buildings": ["religous"],
+        "leisure": True,
+        "shop": True,
+        "highway": ["roads"],
+        "railway": ["stations and stops"],
+        "sport": True
+        }
+
+    pois = ox.geometries_from_bbox(north, south, east, west, tags)
+
+    return pois.groupby("amenity").apply(len).reset_index().set_index("amenity").rename(columns={0:"Count"})
+
+def count_local_features(latitude, longitude, width_km, height_km):
+    box_width = width_km * (0.02/2.2)
+    box_height = height_km * (0.02/2.2)
+
+    north = latitude + (box_height/2)
+    south = latitude - (box_height/2)
+    west = longitude - (box_width/2)
+    east = longitude + (box_width/2)
+
+    tags = {
+        "amenity": True,
+        "buildings": ["religous"],
+        "leisure": True,
+        "shop": True,
+        "highway": ["roads"],
+        "railway": ["stations and stops"],
+        "sport": True
+        }
+
+    pois = ox.geometries_from_bbox(north, south, east, west, tags)
+
+    return len(pois)
+
+def get_rows_in_bounds(north, south, west, east, latest_date, earliest_date, conn):
+    """
+    Get rows from the database according to the specified bounds
+    Return dataframe
+    TODO: Check performance, if slow try to optimize with indices
+    """
+    # Would usually be parameterized to prevent injection
+    # but because there are no user input strings here don't need to worry
+    cur = conn.cursor()
+    cur.execute(f"""
+                SELECT * FROM prices_coordinates_data 
+                WHERE {south} < latitude < {north}
+                AND {east} < longitude > {west}
+                AND CAST('{earliest_date}' as date) < date_of_transfer < CAST('{latest_date}' as date)
+                """)
+    return cur.fetchall()
--- fynesse/address.py
+++ fynesse/address.py
@@ -1,18 +1,146 @@
 # This file contains code for suporting addressing questions in the data
 
-"""# Here are some of the imports we might expect 
-import sklearn.model_selection  as ms
-import sklearn.linear_model as lm
-import sklearn.svm as svm
-import sklearn.naive_bayes as naive_bayes
-import sklearn.tree as tree
-
-import GPy
-import torch
-import tensorflow as tf
-
-# Or if it's a statistical analysis
-import scipy.stats"""
-
 """Address a particular question that arises from the data"""
 
+import pandas as pd
+import pygeohash as gh
+import numpy as np
+import datetime
+import pymysql
+import yaml
+import statsmodels.api as sm
+import access
+
+def predict_price_parameterized(args, latitudes, longitudes, dates, property_types, conn):
+    """
+    Price prediction for UK housing with parameters
+    Returns performance of model with parameters
+    """
+    d, t, h = args
+    d = d * (0.02/2.2)
+    pt = {"days": t}
+    mt = {"days": -t}
+
+    #Finding bounds for latitude and longitude
+    box_width = d * (0.02/2.2)
+    box_height = d * (0.02/2.2)
+    for latitude, longitude, date, property_type in zip(latitudes, longitudes, dates, property_types):
+        north = latitude + (box_height/2)
+        south = latitude - (box_height/2)
+        west = longitude - (box_width/2)
+        east = longitude + (box_width/2)
+
+        #Finding bounds for date
+        latest_date = date + datetime.timedelta(**pt)
+        earliest_date = date + datetime.timedelta(**mt)
+
+        rows = access.get_rows_in_bounds(north, south, west, east, latest_date, earliest_date, conn)
+
+        data = np.vstack(rows)
+        df = pd.DataFrame({
+            "Price": data[:, 1],
+            "Date": data[:, 2],
+            "Property Type": data[:, 3],
+            "Latitude": data[:, -2],
+            "Longitude": data[:, -1]
+        })
+        df["Geohash"] = df.apply(lambda x: gh.encode(x["Latitude"], x["Longitude"], precision=h), axis=1)
+        
+        property_type_oh = np.array(np.array([
+            1 if p == "F" else 0,
+            1 if p == "S" else 0,
+            1 if p == "D" else 0,
+            1 if p == "T" else 0,
+            1 if p == "O" else 0
+            ]) for p in df["Property Type"])
+        geohash_oh = np.array(np.array([
+            1 if g == encoding else 0 for encoding in df["Geohash"].unique()
+        ]) for g in df["Geohash"])
+        np_ord = np.vectorize(lambda x: x.toordinal())
+
+        design = np.concatenate((np_ord(df["Date"]), property_type_oh, geohash_oh), axis=1)
+        print(design)
+
+        m = sm.OLS(y, design)
+        m_results = m.fit()
+        property_oh_pred = np.array([np.array([
+            1 if property_type == "F" else 0,
+            1 if property_type == "S" else 0,
+            1 if property_type == "D" else 0,
+            1 if property_type == "T" else 0,
+            1 if property_type == "O" else 0
+        ])])
+        geohash = gh.encode(latitude, longitude, precision=h)
+        geohash_oh = np.array([np.array([
+            1 if geohash == encoding else 0 for encoding in df["Geohash"].unique()
+        ])])
+        design_pred = np.concatenate(
+            (np.array([date.toordinal()]), property_oh_pred, geohash_oh), axis=1
+        )
+        price_pred = m_results.get_prediction(design_pred)
+        print(price_pred)
+
+def predict_price(latitude, longitude, date, property_type, conn):
+    """
+    Price prediction for UK housing.
+    TODO: Do geohash, Create features matrix, Run regression, Output MSE
+    """
+    
+    #Finding bounds for latitude and longitude
+    box_width = d * (0.02/2.2)
+    box_height = d * (0.02/2.2)
+
+    north = latitude + (box_height/2)
+    south = latitude - (box_height/2)
+    west = longitude - (box_width/2)
+    east = longitude + (box_width/2)
+
+    #Finding bounds for date
+    latest_date = date + datetime.timedelta(**pt)
+    earliest_date = date + datetime.timedelta(**mt)
+
+    rows = access.get_rows_in_bounds(north, south, west, east, latest_date, earliest_date, conn)
+
+    data = np.vstack(rows)
+    df = pd.DataFrame({
+        "Price": data[:, 1],
+        "Date": data[:, 2],
+        "Property Type": data[:, 3],
+        "Latitude": data[:, -2],
+        "Longitude": data[:, -1]
+    })
+    df["Geohash"] = df.apply(lambda x: gh.encode(x["Latitude"], x["Longitude"], precision=h), axis=1)
+    
+    property_type_oh = np.array(np.array([
+        1 if p == "F" else 0,
+        1 if p == "S" else 0,
+        1 if p == "D" else 0,
+        1 if p == "T" else 0,
+        1 if p == "O" else 0
+        ]) for p in df["Property Type"])
+    geohash_oh = np.array(np.array([
+        1 if g == encoding else 0 for encoding in df["Geohash"].unique()
+    ]) for g in df["Geohash"])
+    np_ord = np.vectorize(lambda x: x.toordinal())
+
+    design = np.concatenate((np_ord(df["Date"]), property_type_oh, geohash_oh), axis=1)
+    print(design)
+
+    """
+    m = sm.OLS(y, design)
+    m_results = m.fit()
+    property_oh_pred = np.array(np.array([
+        1 if property_type == "F" else 0,
+        1 if property_type == "S" else 0,
+        1 if property_type == "D" else 0,
+        1 if property_type == "T" else 0,
+        1 if property_type == "O" else 0
+    ]))
+    geohash_oh = np.array(np.array([
+        1 if geohash == encoding for encoding in df["Geohash"].unique()
+    ]))
+    design_pred = np.concatenate(
+        (np.array([date.toordinal()]),)
+    )
+    price_pred = m_results.get_prediction()
+    """
